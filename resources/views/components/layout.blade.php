2812a58c3ff09c0b29f822615bd0d129182136f0
Lukas Krisna Prastiyan <lukas.202201011@student.stikomyos.ac.id>
2024-07-17 01:40:11 +0700

    minor design change

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -36,32 +36,10 @@
 				</form>
 				<ul class="navbar-nav navbar-right">
 					<li class="dropdown">
-						<a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+						<a href="#" data-toggle="dropdown" class="nav-link nav-link-lg nav-link-user">
 							<img alt="image" src="../assets/img/avatar/avatar-1.png" class="rounded-circle mr-1" />
 							<div class="d-sm-none d-lg-inline-block">Halo, {{ auth()->user()->name }}</div>
 						</a>
-						<div class="dropdown-menu dropdown-menu-right">
-							{{-- <div class="dropdown-title">Akun sejak: {{ auth()->user()->diffForHumanDate(auth()->user()->created_at) }}
-							</div>
-							@can('mengatur profile')
-							<a href="{{ route('profile.index') }}" class="dropdown-item has-icon"> <i class="fas fa-cog"></i>
-								Pengaturan Profil </a>
-							@endcan --}}
-							{{-- <div class="dropdown-divider"></div> --}}
-							{{--
-							<a class="dropdown-item has-icon text-danger" href="{{ route('logout') }}">
-								<i class="fas fa-sign-out-alt"></i>
-								{{ __('Logout') }}
-							</a>
-							--}}
-							<form id="logout-form" action="{{ route('logout') }}" method="POST">
-								@csrf
-
-								<button type="submit" class="dropdown-item has-icon btn-link text-danger logout">
-									Logout
-								</button>
-							</form>
-						</div>
 					</li>
 				</ul>
 			</nav>
@@ -69,7 +47,7 @@
 			<div class="main-sidebar">
 				<aside id="sidebar-wrapper">
 					<div class="sidebar-brand">
-						<a href="{{ route('home') }}">{{ config('app.name') }}</a>
+						<a href="{{ route('home') }}">SMP Pius Bakti Utama</a>
 					</div>
 					<div class="sidebar-brand sidebar-brand-sm">
 						<a href="{{ route('home') }}">{{ substr(config('app.name'), 0, 2) }}</a>
